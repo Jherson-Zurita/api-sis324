4647776dd1ec8e193837803521a1a4f80604a445
Jherson-Zurita <103202026+Jherson-Zurita@users.noreply.github.com>
2024-06-19 17:03:02 -0400

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    jwtSecret: 'tu_clave_secreta_para_jwt',
+    dbConnectionString: 'sqlite://database.sqlite'
+    // Agrega otras claves sensibles aquí si es necesario
+  };
+  
